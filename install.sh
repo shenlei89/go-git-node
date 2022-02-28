da16469423c8def1cf33ee75b2fd13cc570149cf
shenlei89 <50196892+shenlei89@users.noreply.github.com>
2022-02-28 13:36:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /usr/local && wget https://golang.google.cn/dl/go1.16.7.linux-amd64.tar.gz -O go1.16.7.linux-amd64.tar.gz
 tar -xvzf go1.16.7.linux-amd64.tar.gz
-wget https://github.com/shenlei89/go-git-node/blob/main/export
+wget https://github.com/shenlei89/go-git-node/blob/main/export.txt
+cat export.txt >> /etc/profile 
