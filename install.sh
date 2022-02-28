5ef9a3a765c395106c0e0201a6421f6fd40911e4
shenlei89 <50196892+shenlei89@users.noreply.github.com>
2022-02-28 13:26:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-./bash
+#!/bin/bash
+cd /usr/local && wget https://golang.google.cn/dl/go1.16.7.linux-amd64.tar.gz -O go1.16.7.linux-amd64.tar.gz
+tar -xvzf go1.16.7.linux-amd64.tar.gz
+echo export GOPROXY=https://goproxy.cn >> /etc/profile 
+echo export GO111MODULE=on >> /etc/profile 
+echo export GOROOT=/usr/local/go >> /etc/profile 
+echo export GOPATH=/usr/local/go/path >> /etc/profile 
