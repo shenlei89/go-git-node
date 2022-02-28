eecdcf0ad2dad7416d358cfbc4bbe42b839a58b7
shenlei89 <50196892+shenlei89@users.noreply.github.com>
2022-02-28 13:51:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,10 +11,15 @@ cd ~
 mkdir /root/sillyGirl
 s=sillyGirl;a=arm64;if [[ $(uname -a | grep "x86_64") != "" ]];then a=amd64;fi ;if [ ! -d $s ];then mkdir $s;fi ;cd $s;wget http://github.yanyuge.workers.dev/https://github.com/cdle/${s}/releases/download/main/${s}_linux_$a -O $s && chmod 777 $s;pkill -9 $s;$(pwd)/
 cd /etc/sillyGirl && wget https://github.com/shenlei89/go-git-node/blob/main/sets.conf
-mkdir /root/node-onebot
-cd /root
 cd /usr/local  && wget https://nodejs.org/dist/v14.17.5/node-v14.17.5-linux-x64.tar.xz
 xz -dk node-v14.17.5-linux-x64.tar.xz  && tar -xvf node-v14.17.5-linux-x64.tar
+cd /root && wget https://github.com/shenlei89/go-git-node/blob/main/e.txt
+cat e.txt >> /etc/profile
+source /etc/profile
+npm install pm2 -g
+cd /root
+git clone https://github.com/takayama-lily/node-onebot.git
+cd node-onebot
 
 
 
